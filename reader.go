9fa33f450b37944edba04d9165ca058a77ab8c7e
wxk6b1203 <wuxk@chinatelecom.cn>
2025-04-03 16:18:22 +0800

    feat: add Reader interface and defaultReader implementation for Kafka message fetching

---
+++ reader.go
@@ -0,0 +1,135 @@
+package sarama
+
+import (
+	"sync"
+	"time"
+
+	"github.com/rcrowley/go-metrics"
+)
+
+type Reader interface {
+	Close() error
+	Config() *Config
+	// Read fetches a batch of messages from the given topic and partition, starting at the given offset.
+	// It returns a FetchResponse containing the messages and any errors encountered during the fetch.
+	Read(topic string, partition int32, offset int64) (*FetchResponse, error)
+}
+
+type defaultReader struct {
+	conf           *Config
+	client         Client
+	metricRegistry metrics.Registry
+	lock           sync.Mutex
+}
+
+func NewReader(addrs []string, config *Config) (Reader, error) {
+	if config == nil {
+		config = NewConfig()
+	}
+	client, err := NewClient(addrs, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &defaultReader{
+		conf:           config,
+		client:         client,
+		metricRegistry: metrics.NewRegistry(),
+	}, nil
+}
+
+func (r *defaultReader) Close() error {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	if r.client != nil {
+		if err := r.client.Close(); err != nil {
+			return err
+		}
+		r.client = nil
+	}
+
+	return nil
+}
+
+func (r *defaultReader) Config() *Config {
+	return r.conf
+}
+
+func (r *defaultReader) makeFetchRequest() (*FetchRequest, error) {
+	tmp := &FetchRequest{
+		MinBytes:    r.conf.Consumer.Fetch.Min,
+		MaxWaitTime: int32(r.conf.Consumer.MaxWaitTime / time.Millisecond),
+	}
+	// Version 1 is the same as version 0.
+	if r.conf.Version.IsAtLeast(V0_9_0_0) {
+		tmp.Version = 1
+	}
+	// Starting in Version 2, the requestor must be able to handle Kafka Log
+	// Message format version 1.
+	if r.conf.Version.IsAtLeast(V0_10_0_0) {
+		tmp.Version = 2
+	}
+	// Version 3 adds MaxBytes.  Starting in version 3, the partition ordering in
+	// the tmp is now relevant.  Partitions will be processed in the order
+	// they appear in the tmp.
+	if r.conf.Version.IsAtLeast(V0_10_1_0) {
+		tmp.Version = 3
+		tmp.MaxBytes = MaxResponseSize
+	}
+	// Version 4 adds IsolationLevel.  Starting in version 4, the reqestor must be
+	// able to handle Kafka log message format version 2.
+	// Version 5 adds LogStartOffset to indicate the earliest available offset of
+	// partition data that can be consumed.
+	if r.conf.Version.IsAtLeast(V0_11_0_0) {
+		tmp.Version = 5
+		tmp.Isolation = r.conf.Consumer.IsolationLevel
+	}
+	// Version 6 is the same as version 5.
+	if r.conf.Version.IsAtLeast(V1_0_0_0) {
+		tmp.Version = 6
+	}
+	// Version 7 adds incremental fetch tmp support.
+	if r.conf.Version.IsAtLeast(V1_1_0_0) {
+		tmp.Version = 7
+		// We do not currently implement KIP-227 FetchSessions. Setting the id to 0
+		// and the epoch to -1 tells the broker not to generate as session ID we're going
+		// to just ignore anyway.
+		tmp.SessionID = 0
+		tmp.SessionEpoch = -1
+	}
+	// Version 8 is the same as version 7.
+	if r.conf.Version.IsAtLeast(V2_0_0_0) {
+		tmp.Version = 8
+	}
+	// Version 9 adds CurrentLeaderEpoch, as described in KIP-320.
+	// Version 10 indicates that we can use the ZStd compression algorithm, as
+	// described in KIP-110.
+	if r.conf.Version.IsAtLeast(V2_1_0_0) {
+		tmp.Version = 10
+	}
+	// Version 11 adds RackID for KIP-392 fetch from closest replica
+	if r.conf.Version.IsAtLeast(V2_3_0_0) {
+		tmp.Version = 11
+		tmp.RackID = r.conf.RackID
+	}
+
+	return tmp, nil
+}
+
+func (r *defaultReader) Read(topic string, partition int32, offset int64) (*FetchResponse, error) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	leader, epoch, err := r.client.LeaderAndEpoch(topic, partition)
+
+	if err != nil {
+		return nil, err
+	}
+
+	req, _ := r.makeFetchRequest()
+
+	req.AddBlock(topic, partition, offset, r.conf.Consumer.Fetch.Max, epoch)
+
+	return leader.Fetch(req)
+}
---
+++ reader_test.go
@@ -0,0 +1,132 @@
+package sarama
+
+import (
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestLoadData(t *testing.T) {
+	config := NewConfig()
+	config.Producer.Flush.Frequency = 3000 * time.Millisecond
+	config.Producer.Compression = CompressionLZ4
+	producer, err := NewAsyncProducer([]string{"localhost:9092"}, config)
+
+	if err != nil {
+		t.Fatalf("Failed to create producer: %v", err)
+	}
+
+	defer producer.Close()
+	for i := 0; i < 10; i++ {
+		now := time.Now()
+		msg := &ProducerMessage{
+			Topic: "test-topic-1",
+			Key:   StringEncoder("data-" + strconv.Itoa(i)),
+			Value: StringEncoder(now.String()),
+		}
+
+		producer.Input() <- msg
+	}
+
+	// Wait for the messages to be sent
+	time.Sleep(5 * time.Second)
+}
+
+func TestTransaction(t *testing.T) {
+	config := NewConfig()
+	config.Producer.Flush.Frequency = 3000 * time.Millisecond
+	config.Producer.Compression = CompressionLZ4
+	config.Producer.Idempotent = true
+	config.Producer.RequiredAcks = WaitForAll
+	config.Net.MaxOpenRequests = 1
+	config.Producer.Transaction.ID = "test-transaction-id-1"
+	producer, err := NewAsyncProducer([]string{"localhost:9092"}, config)
+
+	if err != nil {
+		t.Fatalf("Failed to create producer: %v", err)
+	}
+
+	defer producer.Close()
+
+	// Start a transaction
+	err = producer.BeginTxn()
+	if err != nil {
+		t.Fatalf("Failed to begin transaction: %v", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		now := time.Now()
+		msg := &ProducerMessage{
+			Topic: "test-topic-1",
+			Key:   StringEncoder("txn-" + strconv.Itoa(i)),
+			Value: StringEncoder(now.String()),
+		}
+
+		producer.Input() <- msg
+	}
+
+	for i := range 15 {
+		t.Logf("sleeping %d(th) seconds", i)
+		time.Sleep(time.Second)
+	}
+
+	for i := 0; i < 10; i++ {
+		now := time.Now()
+		msg := &ProducerMessage{
+			Topic: "test-topic-2",
+			Key:   StringEncoder("txn-" + strconv.Itoa(i)),
+			Value: StringEncoder(now.String()),
+		}
+
+		producer.Input() <- msg
+	}
+
+	time.Sleep(3 * time.Second)
+
+	err = producer.CommitTxn()
+	if err != nil {
+		t.Fatalf("Failed to commit transaction: %v", err)
+	}
+
+	time.Sleep(3 * time.Second)
+}
+
+func TestDefaultReader_Read(t *testing.T) {
+	config := NewConfig()
+	reader, err := NewReader([]string{"localhost:9092"}, config)
+	if err != nil {
+		t.Fatalf("Failed to create reader: %v", err)
+	}
+
+	defer reader.Close()
+
+	resp, err := reader.Read("test-topic-1", 0, 122)
+	if err != nil {
+		t.Fatalf("Failed to read message: %v", err)
+	}
+
+	if resp == nil {
+		t.Fatalf("Expected a response, but got nil")
+	}
+
+	for topic, partitionBlocks := range resp.Blocks {
+		for partition, block := range partitionBlocks {
+			if block == nil {
+				t.Fatalf("Expected a block for topic %s, partition %d, but got nil", topic, partition)
+			}
+			for _, r := range block.RecordsSet {
+				off, e := r.recordsOffset()
+				if e != nil {
+					t.Fatalf("Failed to get records offset: %v", e)
+				}
+				if off == nil {
+					t.Fatalf("Expected a records offset, but got nil")
+				}
+				t.Logf("RecordBatch first offset: %d", *off)
+				for _, record := range r.RecordBatch.Records {
+					t.Logf("Read record: delta: %v value: %s", record.OffsetDelta, string(record.Value))
+				}
+			}
+		}
+	}
+}
